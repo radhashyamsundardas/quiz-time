b1aee6495d804ff8706ebbbefa3cdd291655c13f
hiren patel <hirenpatel0526@yahoo.com>
2022-09-16 19:11:41 -0700

    started js

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// Adding questions for the quiz, Questions got from interviewbit.com
+let quizQuestions = [
+    { Question : "Which language is JAVASCRIPT?",
+        Answers:{
+            a: "Object Oriented",
+            b: "Object Based",
+            c: "Procedural",
+            d: "None of Above",
+        }, Correct Answer: " Object Oriented",
+    },
+
+    { Question : "Which of the following keyword is used to define variable in Javascript?"
+        {
+
+        }
+    }
+
+
+
+
+    { 
+        {
+
+        }
+    }
+
+
+
+
+
+
+    { 
+        {
+
+        }
+    }
+]
