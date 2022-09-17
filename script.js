deecfe525bd7e357fc204e95fa573f9c7961d9f8
hiren patel <hirenpatel0526@yahoo.com>
2022-09-16 20:19:16 -0700

    added question for quiz

--- script.js
+++ script.js
@@ -2,37 +2,38 @@
 // Adding questions for the quiz, Questions got from interviewbit.com
 let quizQuestions = [
     { Question : "Which language is JAVASCRIPT?",
-        Answers:{
+        Answer:{
             a: "Object Oriented",
             b: "Object Based",
             c: "Procedural",
             d: "None of Above",
-        }, Correct Answer: " Object Oriented",
+        },  Answer: " Object Oriented",
     },
 
-    { Question : "Which of the following keyword is used to define variable in Javascript?"
-        {
+    { Question : "Which of the following keyword is used to define variable in Javascript?",
+        Answer:{ 
+            a: "var",
+            b: "let",
+            c: "Both A and B",
+            d: "None of above",
+        }, Answer: "Both A and B",
+    },
 
-        }
-    }
+    { Question: "Which METHOD is used to access HTML elements using javascript?",
+        Answer: {
+            a: "getElementById()",
+            b: "getElementByClassname()",
+            c: "Both A and B",
+            d: "None of above",
+        }, Answer: "Both A and B",
+    },
 
-
-
-
-    { 
-        {
-
-        }
-    }
-
-
-
-
-
-
-    { 
-        {
-
-        }
-    }
-]
+   { Question: "How can Datatype be declared to be a constant type",
+        Answer: {
+            a: "const",
+            b: "var",
+            c: "let",
+            d: "constant",
+        }, Answer: "const",
+    },
+];
